3413208bbc54674c1326e15e7ffa59cbdb27155f
sinansubasi <sinan95@gmail.com>
2021-02-11 17:36:41 +0300

    creating and merging development config

---
+++ mfp/packages/marketing/config/webpack.dev.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge'); // merge is a function that we can use to merge together two different web config objects.
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // It takes some kind of HTML file inside of project and inject couple of different script tags inside of it.
+const commonConfig = require('./webpack.common');
+const { module } = require('./webpack.common');
+
+const devConfig = {
+    module: 'development',
+    devServer: {
+        port: 8081,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
